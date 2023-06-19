2b0846d8b9fc272d9310985ebd40195e055b98b2
Paulhdev <paulohenriquerev@gmail.com>
2023-06-19 01:58:52 -0300

    Restyle Design System button and text

--- App.tsx
+++ App.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
+import { ThemeProvider } from '@shopify/restyle';
 
 import { Text } from './src/components/Text/Text';
+import { theme } from './src/assets/theme/theme';
+import { Button } from './src/components/Button/Button';
 
 function App(): JSX.Element {
 
   return (
-    <SafeAreaView style={{ backgroundColor: 'white', flex: 1 }}>
-      <Text>Nubble App</Text>
-    </SafeAreaView>
+    <ThemeProvider theme={theme}>
+      <SafeAreaView style={{ paddingHorizontal: 24 }}>
+
+        <Text italic preset="headingLarge" marginBottom="s16">Nubble App</Text>
+
+        <Button title="Primary" preset="primary" marginBottom="s12" />
+        <Button disabled title="Primary" preset="primary" marginBottom="s12" />
+        <Button title="Outline" preset="outline" marginBottom="s12" />
+
+        <Button title="Loading" loading marginBottom="s12" />
+        <Button disabled title="Loading" loading marginBottom="s12" />
+        <Button disabled preset="outline" title="Loading" loading marginBottom="s12" />
+
+      </SafeAreaView>
+    </ThemeProvider>
   );
 }
 
--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
-import { Text as RNText, TextProps as RNTextProps, TextStyle } from 'react-native';
+import { TextStyle } from 'react-native';
+import { createText } from '@shopify/restyle';
+import { Theme } from '../../assets/theme/theme';
 
-interface TextProps extends RNTextProps {
+const SRText = createText<Theme>();
+
+type SRTextProps = React.ComponentProps<typeof SRText>;
+
+interface TextProps extends SRTextProps {
   preset?: TextVariants;
   bold?: boolean;
   italic?: boolean;
@@ -15,12 +21,12 @@ export function Text({
   italic,
   semiBold,
   style,
-  ...res
+  ...SRTextProps
 }: TextProps) {
 
   const fontFamily = getFontFamily(preset, bold, italic, semiBold);
 
-  return <RNText style={[$fontSizes[preset], { fontFamily }, style]} {...res}>{children}</RNText>;
+  return <SRText color="backgroundContrast" style={[$fontSizes[preset], { fontFamily }, style]} {...SRTextProps}>{children}</SRText>;
 }
 
 function getFontFamily(preset: TextVariants, bold?: boolean, italic?: boolean, semiBold?: boolean) {
